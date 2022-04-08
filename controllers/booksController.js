31f126d3c83a842b27c92a8f97c0a19e244d2e5e
Danwanaa94 <raufyussif04@gmail.com>
2022-04-08 14:29:37 +0000

    first commit

---
+++ controllers/booksController.js
@@ -0,0 +1,31 @@
+const books = require("../books")
+
+//get all books
+
+const getBooks = (req, res)=>{
+    res.status(200).json(books)
+}
+  // delete a book
+  /*const deleteBook =(req, res)=>{
+      const foundBook = await books.findByTitle(req. params.title)
+      if (foundBook){
+          foundBook.remove()
+          res.json({msg:"book removed"})
+      }else{
+          res.status(404).json({error:"Book not found"})
+      }
+  }*/
+
+  //delete result
+  const deleteBook= (req, res)=>{
+    res.json(books.filter((book)=>book.title!==req.params))
+}
+
+
+
+
+
+
+
+
+module.exports={getBooks, deleteBook}
